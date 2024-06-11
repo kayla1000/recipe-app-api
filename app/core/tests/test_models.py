c646e3e000a1865f9dc247057076ca7ce5141186
kayla2000 <orimoloyekayode@outlook.com>
2024-06-10 20:20:17 -0400

    Added tags and updated recipe

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -7,6 +7,11 @@ from decimal import Decimal
 from core import models
 
 
+def create_user(email='user@example.com', password='testpass123'):
+    """Create a return a new user."""
+    return get_user_model().objects.create_user(email, password)
+
+
 class ModelTests(TestCase):
     """Test models"""
     def test_create_user_with_email_successful(self):
@@ -59,4 +64,12 @@ class ModelTests(TestCase):
             price=Decimal('5.50'),
             description='Sample recipe description'
         )
+
         self.assertEqual(str(recipe), recipe.title)
+
+    def test_create_tag(self):
+        """Test creating a tag is successful."""
+        user = create_user()
+        tag = models.Tag.objects.create(user=user, name='Tag1')
+
+        self.assertEqual(str(tag), tag.name)
